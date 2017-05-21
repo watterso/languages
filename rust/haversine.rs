f0090c49672775267bbf8b077c93615a6d5bc153
James Watterson <jameslwatterson@gmail.com>
2017-05-20 17:26:00 -0700

    GeoLocation can calculate a haversine

--- rust/haversine.rs
+++ rust/haversine.rs
@@ -1,9 +1,49 @@
 use std::env;
 
+//a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+//c = 2 ⋅ atan2( √a, √(1−a) )
+//d = R ⋅ c
+//where φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km);
+//note that angles need to be in radians to pass to trig functions!
+fn haversine_f64(delta_lat: f64, lat1: f64, lat2: f64, delta_long:f64) -> f64 {
+    let a = (delta_lat / 2.0f64).sin().powi(2) + lat1.cos() * lat2.cos() * (delta_long / 2.0f64).sin().powi(2);
+    let c = 2.0f64 * a.sqrt().atan2((1.0f64-a).sqrt());
+    let radius_earth_meters = 6371000.0f64;
+    radius_earth_meters * c
+}
+
 #[derive(Debug)]
 struct GeoLocation {
     latitude: f64,
     longitude: f64
+}
+
+#[derive(Debug)]
+struct GeoLocationInRadians {
+    latitude: f64,
+    longitude: f64
+}
+
+fn haversine(point1: GeoLocationInRadians, point2: GeoLocationInRadians) -> f64 {
+    haversine_f64(
+        point1.latitude - point2.latitude,
+        point1.latitude,
+        point2.latitude,
+        point1.longitude - point2.longitude
+    )
+}
+
+impl GeoLocation {
+    fn to_radians(&self) -> GeoLocationInRadians {
+        GeoLocationInRadians{
+            latitude: self.latitude.to_radians(),
+            longitude: self.longitude.to_radians()
+        }
+    }
+
+    fn haversine(&self, other: &GeoLocation) -> f64 {
+        haversine(self.to_radians(), other.to_radians())
+    }
 }
 
 struct GeoLocationBuilder{
@@ -20,7 +60,6 @@ impl ValidCoordinate for f64{
     fn valid_latitude(&self) -> bool { *self >= -90.0f64 && *self <= 90.0f64 }
     fn valid_longitude(&self) -> bool { *self >= -180.0f64 && *self <= 180.0f64 }
 }
-
 
 impl GeoLocationBuilder {
     fn new() -> GeoLocationBuilder {
@@ -54,5 +93,6 @@ fn main() {
     let start = GeoLocationBuilder::new().lat(coords[0]).long(coords[1]).finalize();
     let end = GeoLocationBuilder::new().lat(coords[2]).long(coords[3]).finalize();
     println!("start: {:?}\nend: {:?}", start, end);
+    println!("distance: {} meters", start.haversine(&end));
 
 }
